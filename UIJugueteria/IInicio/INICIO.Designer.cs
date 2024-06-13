7758a17e1dc440575971afcdae431d0424bbf07d
Matías Páez <matiaspaezws@gmail.com>
2024-06-13 20:23:34 -0300

    Agregado de ICONO de juguetería.

--- UIJugueteria/IInicio/INICIO.Designer.cs
+++ UIJugueteria/IInicio/INICIO.Designer.cs
@@ -109,6 +109,7 @@
             this.Controls.Add(this.PanelMovCerrar);
             this.Controls.Add(this.PanelMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "NOSE";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
